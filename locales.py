1de2f060eeae67be1c32880c249384f0cd976ebf
joaojunior321 <116451271+joaojunior321@users.noreply.github.com>
2022-10-31 11:21:43 -0300

    Update locales.py

--- locales.py
+++ locales.py
@@ -1,80 +1,80 @@
 from locales_dict import Locale, LocalesDict
 
-locale_en = Locale()
+locale_pt = Locale()
 locale_ru = Locale()
 locale_uk = Locale()
 locale_de = Locale()
 locale_it = Locale()
 
 locales = LocalesDict({
-    'en': locale_en,
+    'pt': locale_pt,
     'ru': locale_ru,
     'uk': locale_uk,
     'de': locale_de,
     'it': locale_it
-}, locale_en)
+}, locale_pt)
 
 # TOO_LONG_TITLE
-locale_en.too_long_title = 'Your message is too long'
+locale_pt.too_long_title = 'Sua mensagem é muito longa'
 locale_ru.too_long_title = 'Слишком длинное сообщение'
 locale_uk.too_long_title = 'Занадто довге повідомлення'
 locale_de.too_long_title = 'Deine Nachricht ist zu lang'
 locale_it.too_long_title = 'Il tuo messaggio è troppo lungo'
 
 # FOR_TITLE
-locale_en.for_title = 'For %s'
+locale_pt.for_title = 'Para %s'
 locale_ru.for_title = 'Для %s'
 locale_uk.for_title = 'Для %s'
 locale_de.for_title = 'Für %s'
 locale_it.for_title = 'Per %s'
 
 # EXCEPT_TITLE
-locale_en.except_title = 'Except %s'
+locale_pt.except_title = 'Exceto %s'
 locale_ru.except_title = 'Кроме %s'
 locale_uk.except_title = 'Крім %s'
 locale_de.except_title = 'Akzeptiere %s'
 locale_it.except_title = 'Tranne %s'
 
 # SPOILER_TITLE
-locale_en.spoiler_title = 'Spoiler'
+locale_pt.spoiler_title = 'Revelar'
 locale_ru.spoiler_title = 'Спойлер'
 locale_uk.spoiler_title = 'Спойлер'
 locale_de.spoiler_title = 'Spoiler'
 locale_it.spoiler_title = 'Spoiler'
 
 # TOO_LONG_MESSAGE
-locale_en.too_long_message = '🥺 Sorry, your message can\'t be sent as it exceeds the limit of 200 characters.'
+locale_pt.too_long_message = '🥺 Desculpe, sua mensagem não pode ser enviada porque excede o limite de 200 caracteres.'
 locale_ru.too_long_message = '🥺 Ваше сообщение не может быть отправлено, так как его длина превышает лимит в 200 символов.'
 locale_uk.too_long_message = '🥺 Ваше повідомлення не може бути відправлено, так як його довжина перевищує ліміт в 200 символів.'
-locale_de.too_long_message = '🥺 Sorry, deine Nachricht kann nicht gesendet werden, da sie das Limit von 200 Zeichen überschreitet.'
+locale_de.too_long_message = '🥺 Sorry, deine Nachricht kann nicht gesendet werden, da sie das Limit von 200 Zeichptüberschreitet.'
 locale_it.too_long_message = '🥺 Mi dispiace, il tuo messaggio non può essere mandato, supera il limite di 200 caratteri.'
 
 # FOR_MESSAGE
-locale_en.for_message = 'Private message for %s.'
+locale_pt.for_message = 'Mensagem privada para %s.'
 locale_ru.for_message = 'Приватное сообщение для %s.'
 locale_uk.for_message = 'Приватне повідомлення для %s.'
 locale_de.for_message = 'Private Nachricht für %s.'
 locale_it.for_message = 'Messaggio privato per %s.'
 
 # EXCEPT_MESSAGE
-locale_en.except_message = 'Private message for everyone except %s.'
+locale_pt.except_message = 'Mensagem privada para todos, exceto %s.'
 locale_ru.except_message = 'Приватное сообщение для всех, кроме %s.'
 locale_uk.except_message = 'Приватне повідомлення для всіх, крім %s.'
 locale_de.except_message = 'Private Nachricht an alle außer %s.'
 locale_it.except_message = 'Messaggio privato per tutti tranne %s.'
 
 # SPOILER_MESSAGE
-locale_en.spoiler_message = 'Public spoiler message.'
+locale_pt.spoiler_message = 'Mensagem pública revelada.'
 locale_ru.spoiler_message = 'Публичное сообщение под спойлером.'
 locale_uk.spoiler_message = 'Публічне повідомлення під спойлером.'
 locale_de.spoiler_message = 'Öffentlicher Spoiler für alle:'
 locale_it.spoiler_message = 'Messaggio contenente spoiler.'
 
 # GROUP_GREETING_MESSAGE
-locale_en.group_greeting_message = (
-        '👋 Hi! My name is %s and I can help you send private messages that only certain people can view. '
-        'To learn more send /start@%s and feel free to ask for help in our '
-        '<a href="t.me/radzinsky_chat">public community</a> if you\'ve got any questions.')
+locale_pt.group_greeting_message = (
+        '👋 Oi! Meu nome é %s e posso ajudá-lo a enviar mensagens privadas que apenas algumas pessoas podem ver. '
+        'Para saber mais envie /start@%s e sinta-se à vontade para pedir ajuda em nosso '
+        '<a href="t.me/bl4ckoutgroup">comunidade pública</a> se você tiver alguma dúvida.')
 locale_ru.group_greeting_message = (
         '👋 Привет! Меня зовут %s и я могу помочь вам отправлять сообщения, которые смогут прочитать только '
         'определённый круг лиц. Чтобы узнать больше отправьте команду /start@%s и не стесняйтесь просить о помощи '
@@ -93,11 +93,11 @@ locale_it.group_greeting_message = (
         '<a href="t.me/radzinsky_chat">gruppo pubblico</a> se hai domande.')
 
 # INFO_MESSAGE
-locale_en.info_message = (
-        'If you still have questions after reading the article, you can contact support or simply ask '
-        'for help in our public chat at any time you want.\n\n'
-        '👥 Public chat: @radzinsky_chat\n'
-        '⚙ Support: @undrcrxwn')
+locale_pt.info_message = (
+        'Se você ainda tiver dúvidas depois de ler o artigo, entre em contato com o suporte ou simplesmente pergunte '
+        'para obter ajuda em nosso bate-papo público a qualquer momento.\n\n'
+        '👥 Bate-papo público: @bl4ckoutgroup\n'
+        '⚙ Suporte: @BL4CKOUTassistent_bot')
 locale_ru.info_message = (
         'Если у вас остались вопросы после прочтения статьи, вы можете в любое время обратиться в '
         'поддержку или попросить о помощи в нашем публичном чате.\n\n'
@@ -120,42 +120,42 @@ locale_it.info_message = (
          '⚙ Supporto: @undrcrxwn')
 
 # HOW_TO_USE
-locale_en.how_to_use = 'How to use this bot?'
+locale_pt.how_to_use = 'Como usar este bot?'
 locale_ru.how_to_use = 'Как пользоваться этим ботом?'
 locale_uk.how_to_use = 'Як користуватися цим ботом?'
 locale_de.how_to_use = 'Wie geht das?'
 locale_it.how_to_use = 'Come usare questo bot?'
 
 # TOO_LONG_DESCRIPTION
-locale_en.too_long_description = 'Please shorten the length of your message so that it doesn\'t exceed the limit of 200 characters.'
+locale_pt.too_long_description = 'Reduza o comprimento da sua mensagem para que não exceda o limite de 200 caracteres.'
 locale_ru.too_long_description = 'Пожалуйста, сократите длину вашего сообщения, чтобы она не превышала лимит в 200 символов.'
 locale_uk.too_long_description = 'Будь ласка, скоротіть довжину Вашого повідомлення, щоб вона не перевищувала ліміт в 200 символів.'
 locale_de.too_long_description = 'Bitte fasse dich etwas kürzer und überschreite das Limit von 200 Zeichen nicht.'
 locale_it.too_long_description = 'Perfavore accorcia la lunghezza del tuo messaggio in modo che non superi i 200 caratteri.'
 
 # NOT_ALLOWED
-locale_en.not_allowed = 'You are not allowed to view this content.'
+locale_pt.not_allowed = 'Você não tem permissão para visualizar este conteúdo.'
 locale_ru.not_allowed = 'Вам запрещено просматривать этот контент.'
 locale_uk.not_allowed = 'Вам заборонено переглядати цей контент.'
 locale_de.not_allowed = 'Dir ist nicht gestattet, diesen Inhalt zu lesen.'
 locale_it.not_allowed = 'Non hai il permesso per vedere questo messaggio.'
 
 # NOT_ACCESSIBLE
-locale_en.not_accessible = 'This content is no longer accessible.'
+locale_pt.not_accessible = 'Este conteúdo não está mais acessível.'
 locale_ru.not_accessible = 'Этот контент больше недоступен.'
 locale_uk.not_accessible = 'Цей контент більше недоступний.'
 locale_de.not_accessible = 'Der Inhalt ist nicht mehr sichtbar.'
 locale_it.not_accessible = 'Questo contenuto non è più accessibile.'
 
 # VIEW
-locale_en.view = 'View'
+locale_pt.view = 'Ver'
 locale_ru.view = 'Открыть'
 locale_uk.view = 'Відкрити'
 locale_de.view = 'Ansehen'
 locale_it.view = 'Vedi'
 
 # AND_CONNECTOR
-locale_en.and_connector = 'and'
+locale_pt.and_connector = 'e'
 locale_ru.and_connector = 'и'
 locale_uk.and_connector = 'i'
 locale_de.and_connector = '&'
