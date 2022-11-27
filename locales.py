07d3e8f046bc49c2bf38e4001461fef1255998bc
carlosjjjsa2 <115052769+leviobrabo@users.noreply.github.com>
2022-11-27 02:00:31 -0300

    Update locales.py

--- locales.py
+++ locales.py
@@ -15,28 +15,28 @@ locales = LocalesDict({
 }, locale_pt)
 
 # TOO_LONG_TITLE
-locale_pt.too_long_title = '😮 Sua mensagem é muito longa'
+locale_pt.too_long_title = '😮 sᴜᴀ ᴍᴇɴsᴀɢᴇᴍ ᴇ ᴍᴜɪᴛᴏ ʟᴏɴɢᴀ'
 locale_ru.too_long_title = 'Слишком длинное сообщение'
 locale_uk.too_long_title = 'Занадто довге повідомлення'
 locale_de.too_long_title = 'Deine Nachricht ist zu lang'
 locale_it.too_long_title = 'Il tuo messaggio è troppo lungo'
 
 # FOR_TITLE
-locale_pt.for_title = '💬 Enviar mensagem para %s'
+locale_pt.for_title = '💬 ᴇɴᴠɪᴀʀ ᴍᴇɴsᴀɢᴇᴍ ᴘᴀʀᴀ %s'
 locale_ru.for_title = 'Для %s'
 locale_uk.for_title = 'Для %s'
 locale_de.for_title = 'Für %s'
 locale_it.for_title = 'Per %s'
 
 # EXCEPT_TITLE
-locale_pt.except_title = '💬 Enviar mensagem para todos, menos %s'
+locale_pt.except_title = '💬 ᴇɴᴠɪᴀʀ ᴍᴇɴsᴀɢᴇᴍ ᴘᴀʀᴀ ᴛᴏᴅᴏs, ᴍᴇɴᴏs %s'
 locale_ru.except_title = 'Кроме %s'
 locale_uk.except_title = 'Крім %s'
 locale_de.except_title = 'Akzeptiere %s'
 locale_it.except_title = 'Tranne %s'
 
 # SPOILER_TITLE
-locale_pt.spoiler_title = '✅ Revelar mensagem para todos'    
+locale_pt.spoiler_title = '✅ ʀᴇᴠᴇʟᴀʀ ᴍᴇɴsᴀɢᴇᴍ ᴘᴀʀᴀ ᴛᴏᴅᴏs'    
 locale_ru.spoiler_title = 'Спойлер'
 locale_uk.spoiler_title = 'Спойлер'
 locale_de.spoiler_title = 'Spoiler'
@@ -44,8 +44,9 @@ locale_it.spoiler_title = 'Spoiler'
 
 # TOO_LONG_MESSAGE
 locale_pt.too_long_message = (
-        '🥺 Desculpe, sua mensagem não pode ser enviada ' 
-        'porque excede o limite de 200 caracteres.')
+        '🛑🇲 🇪 🇳 🇸 🇦 🇬 🇪 🇲  🇱 🇴 🇳 🇬 🇦 🛑\n\n'
+        '🥺 ᴅᴇsᴄᴜʟᴘᴇ, sᴜᴀ ᴍᴇɴsᴀɢᴇᴍ ɴᴀᴏ ᴘᴏᴅᴇ sᴇʀ ᴇɴᴠɪᴀᴅᴀ ' 
+        'ᴘᴏʀǫᴜᴇ ᴇxᴄᴇᴅᴇ ᴏ ʟɪᴍɪᴛᴇ ᴅᴇ 200 ᴄᴀʀᴀᴄᴛᴇʀᴇs.')
 locale_ru.too_long_message = '🥺 Ваше сообщение не может быть отправлено, так как его длина превышает лимит в 200 символов.'
 locale_uk.too_long_message = '🥺 Ваше повідомлення не може бути відправлено, так як його довжина перевищує ліміт в 200 символів.'
 locale_de.too_long_message = '🥺 Sorry, deine Nachricht kann nicht gesendet werden, da sie das Limit von 200 Zeichptüberschreitet.'
@@ -64,8 +65,9 @@ locale_it.for_message = 'Messaggio privato per %s.'
 
 # EXCEPT_MESSAGE
 locale_pt.except_message = (
-        '💬 %s, você foi exluído(a) desta mensagem privada.\n\n'
-        '🔥 Será que estão falando mal de ti?🤔')
+        '☢️🇲 🇪 🇳 🇸 🇦 🇬 🇪 🇲  🇸 🇪 🇱 🇪 🇹 🇮 🇻 🇦☢️\n\n'
+        '💬 %s, ᴠᴏᴄᴇ ғᴏɪ ᴇxʟᴜɪᴅᴏ(ᴀ) ᴅᴇsᴛᴀ ᴍᴇɴsᴀɢᴇᴍ ᴘʀɪᴠᴀᴅᴀ.\n\n'
+        '🔥 sᴇʀᴀ ǫᴜᴇ ᴇsᴛᴀᴏ ғᴀʟᴀɴᴅᴏ ᴍᴀʟ ᴅᴇ ᴛɪ?🤔')
 locale_ru.except_message = 'Приватное сообщение для всех, кроме %s.'
 locale_uk.except_message = 'Приватне повідомлення для всіх, крім %s.'
 locale_de.except_message = 'Private Nachricht an alle außer %s.'
@@ -73,8 +75,9 @@ locale_it.except_message = 'Messaggio privato per tutti tranne %s.'
 
 # SPOILER_MESSAGE
 locale_pt.spoiler_message = (
-        '👥Mensagem enviada para todos do grupo.\n\n'
-        '👀Agora é sua oportunidade, curioso(a) de plantão!')
+        '📌🇲 🇪 🇳 🇸 🇦 🇬 🇪 🇲  🇵 🇺 🇧 🇱 🇮 🇨 🇦📌'
+        '👥ᴍᴇɴsᴀɢᴇᴍ ᴇɴᴠɪᴀᴅᴀ ᴘᴀʀᴀ ᴛᴏᴅᴏs ᴅᴏ ɢʀᴜᴘᴏ.\n\n'
+        '👀ᴀɢᴏʀᴀ ᴇ sᴜᴀ ᴏᴘᴏʀᴛᴜɴɪᴅᴀᴅᴇ, ᴄᴜʀɪᴏsᴏ(ᴀ) ᴅᴇ ᴘʟᴀɴᴛãᴏ!')
 locale_ru.spoiler_message = 'Публичное сообщение под спойлером.'
 locale_uk.spoiler_message = 'Публічне повідомлення під спойлером.'
 locale_de.spoiler_message = 'Öffentlicher Spoiler für alle:'
@@ -82,9 +85,9 @@ locale_it.spoiler_message = 'Messaggio contenente spoiler.'
 
 # GROUP_GREETING_MESSAGE
 locale_pt.group_greeting_message = (
-        '👋 Oi! Meu nome é %s e posso ajudá-lo a enviar mensagens privadas que apenas algumas pessoas podem ver. '
-        'Para saber mais envie /start@%s e sinta-se à vontade para pedir ajuda em nosso '
-        '<a href="t.me/lbrabo">canal de stickers</a> se você tiver alguma dúvida.')
+        '👋 Oi! 𝕆𝕚! 𝕄𝕖𝕦 𝕟𝕠𝕞𝕖 𝕖 %s 𝕖 𝕡𝕠𝕤𝕤𝕠 𝕒𝕛𝕦𝕕𝕒-𝕝𝕠 𝕒 𝕖𝕟𝕧𝕚𝕒𝕣 𝕞𝕖𝕟𝕤𝕒𝕘𝕖𝕟𝕤 𝕡𝕣𝕚𝕧𝕒𝕕𝕒𝕤 𝕢𝕦𝕖 𝕒𝕡𝕖𝕟𝕒𝕤 𝕒𝕝𝕘𝕦𝕞𝕒𝕤 𝕡𝕖𝕤𝕤𝕠𝕒𝕤 𝕡𝕠𝕕𝕖𝕞 𝕧𝕖𝕣. '
+        'ℙ𝕒𝕣𝕒 𝕤𝕒𝕓𝕖𝕣 𝕞𝕒𝕚𝕤 𝕖𝕟𝕧𝕚𝕖 /start@%s 𝕖 𝕤𝕚𝕟𝕥𝕒-𝕤𝕖 à 𝕧𝕠𝕟𝕥𝕒𝕕𝕖 𝕡𝕒𝕣𝕒 𝕡𝕖𝕕𝕚𝕣 𝕒𝕛𝕦𝕕𝕒 𝕖𝕞 𝕟𝕠𝕤𝕤𝕠 '
+        '<a href="t.me/lbrabo">𝕔𝕒𝕟𝕒𝕝 𝕕𝕖 𝕤𝕥𝕚𝕔𝕜𝕖𝕣𝕤</a> ℙ𝕠𝕕𝕖 𝕣𝕠𝕦𝕓𝕒𝕣 𝕢𝕦𝕒𝕟𝕥𝕒𝕤 𝕗𝕚𝕘𝕦𝕣𝕚𝕟𝕙𝕒𝕤 𝕢𝕦𝕚𝕤𝕖𝕣!')
 locale_ru.group_greeting_message = (
         '👋 Привет! Меня зовут %s и я могу помочь вам отправлять сообщения, которые смогут прочитать только '
         'определённый круг лиц. Чтобы узнать больше отправьте команду /start@%s и не стесняйтесь просить о помощи '
@@ -104,40 +107,40 @@ locale_it.group_greeting_message = (
 
 # INFO_MESSAGE
 locale_pt.info_message = (
-        'Se você ainda tiver dúvidas depois de ler o artigo, entre em contato com o suporte ou simplesmente pergunte '
-        'para obter ajuda em nosso bate-papo público a qualquer momento.\n\n'
-        '👥 Bate-papo público: Não tem\n'
-        '⚙ Suporte: @kylorensBot')
+        'sᴇ ᴠᴏᴄᴇ ᴀɪɴᴅᴀ ᴛɪᴠᴇʀ ᴅᴜᴠɪᴅᴀs ᴅᴇᴘᴏɪs ᴅᴇ ʟᴇʀ ᴏ ᴀʀᴛɪɢᴏ, ᴇɴᴛʀᴇ ᴇᴍ ᴄᴏɴᴛᴀᴛᴏ ᴄᴏᴍ ᴏ sᴜᴘᴏʀᴛᴇ ᴏᴜ sɪᴍᴘʟᴇsᴍᴇɴᴛᴇ ᴘᴇʀɢᴜɴᴛᴇ '
+        'ᴘᴀʀᴀ ᴏʙᴛᴇʀ ᴀᴊᴜᴅᴀ ᴇᴍ ɴᴏssᴏ ʙᴀᴛᴇ-ᴘᴀᴘᴏ ᴘúʙʟɪᴄᴏ ᴀ ǫᴜᴀʟǫᴜᴇʀ ᴍᴏᴍᴇɴᴛᴏ.\n\n'
+        '👥 ᴄᴀɴᴀʟ: @ʟʙʀᴀʙᴏ\n'
+        '⚙ sᴜᴘᴏʀᴛᴇ: @ᴋʏʟᴏʀᴇɴsʙᴏᴛ')
 locale_ru.info_message = (
         'Если у вас остались вопросы после прочтения статьи, вы можете в любое время обратиться в '
         'поддержку или попросить о помощи в нашем публичном чате.\n\n'
-        '👥 Публичный чат: NO\n'
+        '👥 Публичный чат: @ʟʙʀᴀʙᴏ\n'
         '⚙ Поддержка: @kylorensBot')
 locale_uk.info_message = (
         'Якщо у вас залишилися питання після прочитання статті, ви можете в будь-який час звернутися в службу '
         'підтримки або попросити про допомогу в нашому публічному чаті.\n\n'
-        '👥 Публічний чат: No\n'
+        '👥 Публічний чат: @ʟʙʀᴀʙᴏ\n'
         '⚙ Підтримка: @kylorensBot')
 locale_de.info_message = (
         'Wenn du nach dem Lesen des Artikels noch Fragen hast, kannst du den Support kontaktieren oder einfach '
         'in unserem öffentlichen Chat um Hilfe bitten, wann immer du willst.\n\n'
-        '👥 öffentlichen Chat: No\n'
+        '👥 öffentlichen Chat: @ʟʙʀᴀʙᴏ\n'
         '⚙ Hilfe: @kylorensBot')
 locale_it.info_message = (
          'Se hai ancora domande dopo aver letto questo articolo, puoi contattare il supporto nella nostra '
          'chat pubblica quando vuoi.\n\n'
-         '👥 Gruppo pubblico: No\n'
+         '👥 Gruppo pubblico: @ʟʙʀᴀʙᴏ\n'
          '⚙ Supporto: @kylorensBot')
 
 # HOW_TO_USE
-locale_pt.how_to_use = '🤖Como usar este bot?🤖'
+locale_pt.how_to_use = '🤖ᴄᴏᴍᴏ ᴜsᴀʀ ᴇsᴛᴇ ʙᴏᴛ?🤖'
 locale_ru.how_to_use = 'Как пользоваться этим ботом?'
 locale_uk.how_to_use = 'Як користуватися цим ботом?'
 locale_de.how_to_use = 'Wie geht das?'
 locale_it.how_to_use = 'Come usare questo bot?'
 
 # TOO_LONG_DESCRIPTION
-locale_pt.too_long_description = 'Reduza o tamanho da sua mensagem para que não exceda o limite de 200 caracteres.🤏'
+locale_pt.too_long_description = 'ʀᴇᴅᴜᴢᴀ ᴏ ᴛᴀᴍᴀɴʜᴏ ᴅᴀ sᴜᴀ ᴍᴇɴsᴀɢᴇᴍ ᴘᴀʀᴀ ǫᴜᴇ ɴᴀᴏ ᴇxᴄᴇᴅᴀ ᴏ ʟɪᴍɪᴛᴇ ᴅᴇ 200 ᴄᴀʀᴀᴄᴛᴇʀᴇs.🤏'
 locale_ru.too_long_description = 'Пожалуйста, сократите длину вашего сообщения, чтобы она не превышала лимит в 200 символов.'
 locale_uk.too_long_description = 'Будь ласка, скоротіть довжину Вашого повідомлення, щоб вона не перевищувала ліміт в 200 символів.'
 locale_de.too_long_description = 'Bitte fasse dich etwas kürzer und überschreite das Limit von 200 Zeichen nicht.'
@@ -145,29 +148,29 @@ locale_it.too_long_description = 'Perfavore accorcia la lunghezza del tuo messag
 
 # NOT_ALLOWED
 locale_pt.not_allowed = (
-        '⚜️Você é curioso(a) de mais ein!⚜️\n\n'
-        '⚠️Você não tem permissão para visualizar este conteúdo⚠️')
+        '⚜️ᴠᴏᴄᴇ ᴇ ᴄᴜʀɪᴏsᴏ(ᴀ) ᴅᴇ ᴍᴀɪs ᴇɪɴ!⚜️\n\n'
+        '⚠️ᴠᴏᴄê ɴãᴏ ᴛᴇᴍ ᴘᴇʀᴍɪssãᴏ ᴘᴀʀᴀ ᴠɪsᴜᴀʟɪᴢᴀʀ ᴇsᴛᴇ ᴄᴏɴᴛᴇᴜᴅᴏ⚠️')
 locale_ru.not_allowed = 'Вам запрещено просматривать этот контент.'
 locale_uk.not_allowed = 'Вам заборонено переглядати цей контент.'
 locale_de.not_allowed = 'Dir ist nicht gestattet, diesen Inhalt zu lesen.'
 locale_it.not_allowed = 'Non hai il permesso per vedere questo messaggio.'
 
 # NOT_ACCESSIBLE
-locale_pt.not_accessible = '⛔ Este conteúdo não está mais acessível ⛔'
+locale_pt.not_accessible = '⛔ᴇsᴛᴇ ᴄᴏɴᴛᴇúᴅᴏ ɴãᴏ ᴇsᴛᴀ ᴍᴀɪs ᴀᴄᴇssɪᴠᴇʟ⛔'
 locale_ru.not_accessible = 'Этот контент больше недоступен.'
 locale_uk.not_accessible = 'Цей контент більше недоступний.'
 locale_de.not_accessible = 'Der Inhalt ist nicht mehr sichtbar.'
 locale_it.not_accessible = 'Questo contenuto non è più accessibile.'
 
 # VIEW
-locale_pt.view = '⭐ Ver ⭐'
+locale_pt.view = '⭐ 𝕍𝕖𝕣 ⭐'
 locale_ru.view = 'Открыть'
 locale_uk.view = 'Відкрити'
 locale_de.view = 'Ansehen'
 locale_it.view = 'Vedi'
 
 # AND_CONNECTOR
-locale_pt.and_connector = 'e'
+locale_pt.and_connector = 'ᴇ'
 locale_ru.and_connector = 'и'
 locale_uk.and_connector = 'i'
 locale_de.and_connector = '&'
